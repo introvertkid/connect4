ac627399d0ef8a13f589a3a67bf2c31abcce3157
introvertkid <phvu1403@gmail.com>
2025-03-08 19:21:44 +0700

    Planning

---
+++ Board.py
@@ -0,0 +1,14 @@
+class Board:
+    COL = 7
+    ROW = 6
+    MOV = ""
+
+    def __init__(self):
+        pass
+
+    def isValid(col):
+        return True
+    
+    def isWinningMove(col):
+        return True
+
