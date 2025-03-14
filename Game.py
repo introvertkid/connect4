83c936909bc8379ba987d9955af24c51394a0680
SonLeUet <sonle.25.lsts@gmail.com>
2025-03-14 13:46:53 +0700

    newbutton click + show player win

--- Game.py
+++ Game.py
@@ -13,9 +13,10 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 VIOLET = (238,130,238)
 NAVY = (0,0,128)
+YELLOW = (255, 255, 0)
+RED = (255, 0, 0)
 
 font = pygame.font.SysFont("comic sans ms", 40)
-
 clock = pygame.time.Clock()
 
 screen = pygame.display.set_mode((1000, 600))
@@ -27,6 +28,36 @@ gameState = True
 coordinates = []
 
 board = Board.Board()
+
+win_flag = False
+
+def reset_game():
+    global board, coordinates, win_flag 
+    board = Board.Board()  
+    coordinates = [] 
+    win_flag = False 
+    screen.fill(GREY)
+    
+    rect1 = pygame.Rect(50, 100, 200, 50)
+    
+    pygame.draw.rect(screen, WHITE, rect1)
+    
+    rect2 = pygame.Rect(50, 180, 200, 50)
+    pygame.draw.rect(screen, WHITE, rect2)
+    
+    for i in range (0, 601, 100):
+        pygame.draw.line(screen, WHITE, (300, i), (1000, i), 3)
+    for i in range (300, 1001, 100):
+        pygame.draw.line(screen, WHITE, (i, 0), (i, 600), 3)
+
+    text1 = font.render("NEW", True, BLACK)  
+    text1_rect = text1.get_rect(center=rect1.center)  
+    screen.blit(text1, text1_rect) 
+    
+    text2 = font.render("BACK", True, BLACK)
+    text2_rect = text2.get_rect(center=rect2.center)
+    screen.blit(text2, text2_rect)
+    
 
 def draw_background():
     screen.fill(GREY)
@@ -66,7 +97,7 @@ def draw_background():
 while gameState:
     mouseX = pygame.mouse.get_pos()[0]
     mouseY = pygame.mouse.get_pos()[1]
-    isMoved = False
+    isMoved = False 
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -74,6 +105,9 @@ while gameState:
             exit()
 
         if event.type == pygame.MOUSEBUTTONUP:
+            if pygame.Rect(50, 100, 200, 50).collidepoint(event.pos):  # Nếu nhấn vào nút "NEW"
+                reset_game()
+                pygame.display.flip() 
             x = math.floor((mouseX - 300) / 100)
             y = board.play(x)
 
@@ -87,10 +121,18 @@ while gameState:
             # check win
             if board.isWinningMove():
                 print(f"{board.current_player} WIN!")
-
+                win_flag = True
             board.current_player ^= 1
     
     draw_background()
+
+    if(win_flag):
+                win_block = pygame.Rect(20, 300, 250, 100)
+                pygame.draw.rect(screen, YELLOW, win_block)
+                win_tect = font.render("Player "+str(board.current_player+1)+" win!", True, RED)
+                win_rect = win_tect.get_rect(center=win_block.center)
+                screen.blit(win_tect, win_rect)
+                pygame.display.update()
                     
     pygame.display.flip()
     clock.tick(120)
