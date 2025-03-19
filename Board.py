252643f9a2403aee4f7faf37a6f91ec9c31dff4e
introvertkid <phvu1403@gmail.com>
2025-03-19 20:46:39 +0700

    Added Minimax solver with Alpha-Beta pruning; Added undo feature

--- Board.py
+++ Board.py
@@ -1,12 +1,62 @@
-class Board:
-    COL = 7
-    ROW = 6
-    MOV = ""
+import copy
 
-    def __init__(self):
+class Board:
+    ROW = 6
+    COL = 7
+
+    def __init__(self, Row = 6, Col = 7, Mov = ""):
         self.board = [[' ' for _ in range(self.COL)] 
-                        for _ in range(self.ROW)] 
+                        for _ in range(self.ROW)]
+        self.ROW = Row
+        self.COL = Col
         self.current_player = 0
+        self.MOV = Mov
+
+    def copy(self):
+        return copy.deepcopy(self)
+
+    def minimax(self, lo, hi):
+        # check for draw game
+        if len(self.MOV) == self.ROW * self.COL:
+            return 0
+        
+        for x in range(self.COL):
+            if self.play(x) != -1:
+                if not self.isWinningMove():
+                    self.undoMove()
+                    return (self.ROW * self.COL + 1 - len(self.MOV)) / 2
+                            
+        # upper bound of our score as we can't win immediately
+        mx = (self.ROW * self.COL - 1 - len(self.MOV)) / 2
+
+        if hi > mx:
+            hi = mx
+            if lo >= hi:
+                return hi
+            
+        for x in range(self.COL):
+            if(self.isValid(x)):
+                P2 = self.copy()
+                P2.play(x)
+                score = -P2.minimax(-hi, -lo)
+
+                if score >= hi:
+                    return score
+                if score > lo: 
+                    lo = score
+
+        return lo
+
+    def undoMove(self):
+        if len(self.MOV):
+            c = self.MOV[-1]
+            self.MOV = self.MOV[:-1]
+            for r in range(self.ROW):
+                if self.board[r][c] != ' ':
+                    self.board[r][c] = ' '
+                    return
+        else:
+            print('Unable to undo move')
 
     def isValid(self, col):
         return 0 <= col < self.COL and self.board[0][col] == ' '
@@ -20,6 +70,7 @@ class Board:
         for row in reversed(range(self.ROW)):
             if self.board[row][col] == ' ':
                 self.board[row][col] = self.current_player
+                self.MOV += str(col)
                 return row
 
     def isWinningMove(self):
